2ebd0d1e3bee3aaaa6b470b6b9f0600732fb69e0
Matvey <aventadoroqwerty@gmail.com>
2022-03-08 18:35:51 -0500

    microcode.json maker v1

--- Microcode.py
+++ Microcode.py
@@ -1,10 +1,11 @@
+import jsbeautifier as jsbeautifier
 from intelhex import IntelHex
 import json
 
 
 class Microcode:
     def __init__(self):
-        self.microcode: IntelHex = None
+        self.rom: IntelHex = None
 
     def load_rom_file(self, filename: str = None):
         if filename is None:
@@ -13,14 +14,25 @@ class Microcode:
         if not filename.endswith('.hex'):
             filename += '.hex'
 
-        self.microcode = IntelHex(filename)
+        self.rom = IntelHex(filename)
 
     def export(self, filename: str = None) -> None:
         if filename is None:
-            filename = 'microcode.json'
+            filename = 'rom.json'
 
         if not filename.endswith('.json'):
             filename += '.json'
+
+        with open(filename, 'w+') as file:
+            # TODO:  self.rom.tobinarray().tolist() kinda memory-inefficient.
+            #  Find a way to serialize array to JSON
+            microcode_json = {'rom': self.rom.tobinarray().tolist()}
+
+            beautifier_options = jsbeautifier.default_options()
+            beautifier_options.indent_size = 2
+            beautifier_options.wrap_line_length = 120
+
+            file.write(jsbeautifier.beautify(json.dumps(microcode_json), beautifier_options))
 
 
 def text_rom_to_hex(txt_filename: str = None, hex_filename: str = None):
@@ -43,5 +55,4 @@ def text_rom_to_hex(txt_filename: str = None, hex_filename: str = None):
                 ih[idx] = int(byte)
                 idx += 1
 
-    # with open(hex_filename, 'w+') as file:
     ih.write_hex_file(hex_filename)
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from Microcode import Microcode
+
+
 def main():
     pass
 
@@ -5,6 +8,10 @@ def main():
 def setup_tools():
     from Microcode import text_rom_to_hex
     text_rom_to_hex('datafiles/rom.txt')
+
+    mc = Microcode()
+    mc.load_rom_file('datafiles/rom.hex')
+    mc.export('datafiles/microcode.json')
 
 
 if __name__ == '__main__':
